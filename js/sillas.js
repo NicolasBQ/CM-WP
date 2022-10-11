c5452b08dc3954acc2ca1d6167a03fd659ea85fe
NicolasBQ <nicolasbquintero@gmail.com>
2022-10-11 13:20:45 -0500

    Gallery Update

--- js/sillas.js
+++ js/sillas.js
@@ -1,27 +1,17 @@
-const swiper_wrapper = document.querySelector('.swiper-wrapper');
+const grid_container = document.querySelector('.grid-container');
 
 
 const loadGallery = () => {
     for(let i = 1; i < 29; i++) {
-        swiper_wrapper.innerHTML += 
+        grid_container.innerHTML += 
         `
-            <div class="swiper-slide">
-    <img src="assets/GalleryImgs/ImgSillas/silla_${i}.jpg" class="swiper-lazy gallery-img" loading="lazy"/>
+            <div class="grid-element">
+    <img src="assets/GalleryImgs/ImgSillas/silla_${i}.jpg" class="gallery-img" loading="lazy"/>
             </div>
         `
     };
 
-    const swiper = new Swiper('.swiper', {
-        lazy: true,
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }
-    })
+
 }
 
 
